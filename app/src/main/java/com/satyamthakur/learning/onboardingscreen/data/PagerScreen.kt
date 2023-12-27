2b279cdd8cc28b8c64b4e5c0b51e4672e0febfc9
Satyam Thakur <sat.thakur20@gmail.com>
2023-12-27 23:24:22 +0530

    Creating Pager Screen

---
+++ app/src/main/java/com/satyamthakur/learning/onboardingscreen/data/PagerScreen.kt
@@ -0,0 +1,87 @@
+package com.satyamthakur.learning.onboardingscreen.data
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.satyamthakur.learning.onboardingscreen.R
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PagerScreen(items: List<OnboardingData>) {
+    val pagerState = rememberPagerState(pageCount = { items.size })
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+
+}
+
+@Composable
+fun PageView(item: OnboardingData) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = item.image),
+            contentDescription = null,
+            modifier = Modifier.height(250.dp),
+            alignment = Alignment.Center
+        )
+
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Text(
+            text = item.title,
+            fontSize = 32.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = item.description,
+            fontSize = 16.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Normal,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Prev() {
+    PageView(
+        item = OnboardingData(
+            image = R.drawable.saly_03,
+            title = "Lorem Ipsum",
+            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        )
+    )
+}
